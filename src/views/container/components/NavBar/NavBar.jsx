e03d8ca63e810e930f428eee489e4e96a794f0a0
cleon98 <c.l3on98@gmail.com>
2022-04-07 19:16:40 -0500

    burger menu

--- src/views/container/components/NavBar/NavBar.jsx
+++ src/views/container/components/NavBar/NavBar.jsx
@@ -1,22 +1,59 @@
-import React from 'react'
-import styles from './styles.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./styles.module.css";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <nav className={styles.NavBar}>
-      <Link to='/'>
-        <img src='https://i.imgur.com/MwoLWpn.png' className={styles.navLogo} alt='Store logo'/></Link>
-        <div className={styles.navSymbols} >
-        <label for='toggle'> &#9776; </label>
-        <Link to='/catalogo' className={styles.navList} >Catálogo</Link>
-        <Link to='/catalogo/hombre' className={styles.navList} >Hombre</Link>
-        <Link to='/catalogo/mujer' className={styles.navList} >Mujer</Link>
-        <Link to='/cart'> <img src="https://i.imgur.com/XlOLBHJ.png" alt="Shopping cart icon" className={styles.navCart}/> </Link>
+    <div>
+      <nav>
+        <input type="checkbox" className={styles.check} id="check"></input>
+        <label htmlFor="check" className={styles.checkbtn}>
+          <i className="fas fa-bars"></i>{" "}
+        </label>
+        <Link to="/cart">
+          {" "}
+          <img
+            src="https://i.imgur.com/XlOLBHJ.png"
+            alt="Shopping cart icon"
+            className={styles.navCart}
+          />{" "}
+        </Link>
+        <Link className={styles.link} to="/">
+          <img
+            src="https://i.imgur.com/MwoLWpn.png"
+            className={styles.navLogo}
+            alt="Store logo"
+          />
+        </Link>
+        <div className={styles.cart} >
+
         </div>
-    </nav>
+        
+        <ul>
+          <li htmlFor="check" >
+            {" "}
+            <Link to="/catalogo" className={styles.navList}>
+              Catálogo
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/catalogo/hombre" className={styles.navList}>
+              Hombre
+            </Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/catalogo/mujer" className={styles.navList}>
+              Mujer
+            </Link>
+          </li>
+        </ul>
 
-  )
-}
+      </nav>
+  
+    </div>
+  );
+};
 
-export default NavBar
+export default NavBar;
